e85b69d7fd0403a3e1dadeba891b9afa10e24126
Allison Tarr <hello@allisontarr.com>
2015-05-23 15:09:28 -0400

    these aren't the droids you're looking for

--- 404.php
+++ 404.php
@@ -3,9 +3,14 @@
 <div class="main">
   <div class="container">
 
-    <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
+		<div class="four-oh-four">
+	    <h1>Uh oh.</h1>
+	    <h2>These aren't the droids you're looking for.</h2>
+	    <h3>Maybe try again from the <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+          homepage</a>?</h3>
+	    <img src="<?php bloginfo('template_url')?>/img/droids.png" alt="Several Star Wars droids">
+
+    </div>
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
