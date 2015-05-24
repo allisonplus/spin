958c4881414b7a40a8273af6d52dab708aa650fa
Allison Tarr <hello@allisontarr.com>
2015-05-24 14:12:42 -0400

    fidget prev-next link loop

--- loop.php
+++ loop.php
@@ -54,10 +54,10 @@
 
 <?php endwhile; // End the loop. Whew. ?>
 
-<div class="left-right">
-	<?php // Display navigation to next/previous pages when applicable ?>
+<div class="left-right clearfix">
+	<?php // Display navigation to next-previous pages when applicable ?>
 	<?php if (  $wp_query->max_num_pages > 1 ) : ?>
 	  <p class="alignleft"><?php next_posts_link('&laquo; Older Entries'); ?></p>
 	  <p class="alignright"><?php previous_posts_link('Newer Entries &raquo;'); ?></p>
 	<?php endif; ?>
-</div> <!--/entries-->
+</div> <!--/.left-right-->
