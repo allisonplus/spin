1a39368d5baff05540fb168d4b60e4979202bb19
Allison Tarr <hello@allisontarr.com>
2015-05-26 18:47:01 -0400

    addition of contact form & styling

--- page.php
+++ page.php
@@ -4,11 +4,14 @@
   <div class="container">
 
     <div class="page-content">
+
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <h2><?php the_title(); ?></h2>
+
         <?php the_content(); ?>
+
 
       <?php endwhile; // end the loop?>
     </div> <!-- /,content -->
