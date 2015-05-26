f96da4404b09efac85da01dd11e7b11bbcbaa5d3
Allison Tarr <hello@allisontarr.com>
2015-05-25 20:41:17 -0400

    default image styling adjust

--- functions.php
+++ functions.php
@@ -34,13 +34,12 @@ function theme_setup() {
 	return $newsizes;
 	}
 
-	// Function to remove <p> tags on images
+	// function to remove <p> tags on images
 	function filter_ptags_on_images($content){
 	    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 	}
 	add_filter('the_content', 'filter_ptags_on_images');
-
-	// Function to set default image link to none
+	// function to set default image link to none
 	update_option('image_default_link_type','none');
 
 	// Add default posts and comments RSS feed links to head
