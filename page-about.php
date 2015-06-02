779cbdd437c84464882804cf11f67cbb4e6e82dd
Allison Tarr <hello@allisontarr.com>
2015-06-02 18:51:55 -0400

    about custom field repeater

--- page-about.php
+++ page-about.php
@@ -6,9 +6,14 @@
     <div class="page-content">
 
     <div class="about-photo">
-          <?php $bio = get_field('about_photo'); ?>
-          <img src="<?php echo $bio['sizes']['square']; ?> " alt="">
-        </div>
+          <?php while( has_sub_field('images') ): ?>
+
+          <div class="grid js-masonry image">
+            <?php $image = get_sub_field('image'); ?>
+            <img class="grid-item grid-item--width2" src="<?php echo $image['sizes']['sp-full'] ?>">
+          </div>
+        <?php endwhile; ?>
+    </div>
 
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
