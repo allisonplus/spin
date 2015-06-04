489f4c2bb5961c5d2a6c9b3ff1f4c71d1b9a78f3
Allison Tarr <hello@allisontarr.com>
2015-06-04 19:20:18 -0400

    about page stylin'

--- page-about.php
+++ page-about.php
@@ -5,6 +5,11 @@
 
     <div class="page-content-about">
 
+      <!-- <h2>Yours Truly</h2> -->
+      <div class="user-photo">
+        <?php $image = get_field('user-image'); ?>
+        <img class="photo" src="<?php echo $image['sizes']['one-hundred'] ?>">
+      </div> <!--/.user-photo-->
       <div class="info">
         <?php // Start the loop ?>
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
@@ -14,10 +19,6 @@
         <?php endwhile; // end the loop?>
       </div> <!--/.info-->
 
-      <div class="user-photo">
-        <?php $image = get_field('user-image'); ?>
-        <img class="photo" src="<?php echo $image['sizes']['one-hundred'] ?>">
-      </div> <!--/.user-photo-->
     </div> <!-- /.page-content-about -->
 
   </div> <!-- /.container -->
