6929195318f293af729d5826dd6efe09d3e1f747
Allison Tarr <hello@allisontarr.com>
2015-06-04 09:48:15 -0400

    be the loop you want to see in the world

--- home.php
+++ home.php
@@ -6,22 +6,28 @@
   <div class="container">
 
     <div class="bp-content clearfix">
-  		<?php $mainQuery = new WP_Query(
-    		array( 
-    				// 'posts_per_page' => 6,
-    			'showposts'=> 2,
-    				) 
-  		); ?>
 
+    <?php if ( ! have_posts() ) : ?>
 
-  		<?php if ( $mainQuery->have_posts() ) : ?>
+    	<article id="post-0" class="post error404 not-found">
+    		<h1 class="entry-title">Not Found</h1>
+    		<section class="entry-content">
+    			<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
+    			<?php get_search_form(); ?>
+    		</section><!-- .entry-content -->
+    	</article><!-- #post-0 -->
 
-  			<?php while ( $mainQuery->have_posts() ) : $mainQuery->the_post(); ?>
-  				<?php 
-						$thumb_id = get_post_thumbnail_id();
-						$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'highlighted', true);
-						$thumb_url = $thumb_url_array[0];
-  				?>
+    <?php endif; // end if there are no posts ?>
+
+    <?php // if there are posts, Start the Loop. ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+    		<?php 
+    			$thumb_id = get_post_thumbnail_id();
+    			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'highlighted', true);
+    			$thumb_url = $thumb_url_array[0];
+    		?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -31,41 +37,38 @@
 				<a class="post-link" href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark"></a>
 				
 				<div class="bp-thumbnail-header">
-					<h2 class="entry-title">
-					  <a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark">
-					    <?php the_title(); ?>
-					  </a>
-					</h2>
 					<div class="meta-data">
-						<span class="entry-cat" id="entry-cat">
-							<?php 
-							$category = get_the_category(); 
-							if($category[0]){
-							echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
-							} ?>
-						</span>
-						<span class="entry-date"><i class="fa fa-clock-o"></i><?php the_date('F jS, Y', '<p>', '</p>'); ?></span>
-					</div> <!--/.meta-data-->
+							<span class="entry-cat" id="entry-cat">
+								<?php 
+								$category = get_the_category(); 
+								if($category[0]){
+								echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
+								} ?>
+							</span>
+							<span class="entry-date"><i class="fa fa-clock-o"></i><?php the_date('F jS, Y', '<p>', '</p>'); ?></span>
+
+						</div> <!--/.meta-data-->
+			      <h2 class="entry-title">
+			        <a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark">
+			          <?php the_title(); ?>
+			        </a>
+			      </h2>
 		      <?php the_excerpt(); ?>
 
 		    </div> <!--/.bp-thumbnail-header-->
 
 			</article><!-- #post-## -->
-  			<?php endwhile; ?>
 
-  				<?php wp_reset_postdata(); ?>
+			<?php comments_template( '', true ); ?>
 
+  	<?php endwhile; // End the loop. Whew. ?>
 
-  		<?php else:  ?>
-  			
-  		<?php endif; ?>
+		<?php // Display navigation to next/previous pages when applicable ?>
+		<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+		  <p class="alignleft"><?php next_posts_link("&laquo; Older Entries"); ?></p>
+		  <p class="alignright"><?php previous_posts_link("Newer Entries &raquo;"); ?></p>
+		<?php endif; ?>
   		
-  				<?php // Display navigation to next/previous pages when applicable ?>
-  				<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-  				  <p class="alignleft"><?php next_posts_link("&laquo; Older Entries"); ?></p>
-  				  <p class="alignright"><?php previous_posts_link("Newer Entries &raquo;"); ?></p>
-  				<?php endif; ?>
-
     </div> <!--/.content -->
 
   </div> <!-- /.container -->
